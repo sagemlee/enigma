cc800c4946e6e753ba794a7dce70e480f11fff46
Sage Lee <sagemcklee@gmail.com>
2020-04-19 11:49:46 -0600

    Add work to organize files and classes

---
+++ lib/cipher.rb
@@ -0,0 +1,36 @@
+
+
+def initialize(message, key, date)
+  @ message =
+  @alphabet
+  keys = Key.new
+  offset = Offset.new
+end
+
+
+def keys(key)
+  #take in random number
+  #return hash A-D
+end
+
+def offset(date)
+  #take in date
+  #return hash A-D
+end
+
+def shift(key,date)
+  keys(key) + offset(date)
+  return a hash of shifts
+
+end
+
+def change_alphabet_with_shift
+  #change the @alphabet accordin to the shift
+  #will use idex and modulos
+@changed_alphabet =  @alphabet.map |letter|
+  if letter index modulo == 2 , letter + shift[a]
+
+def new message
+  #take every letter from message and change it according to changed alphabet
+
+  end
