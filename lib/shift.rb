736653541a57d576b475630210a888116f8add46
Sage Lee <sagemcklee@gmail.com>
2020-04-19 12:24:17 -0600

    Add keys method and test

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,2 +1,21 @@
 #hash of A = {key => something, offset => somthing}
 #key and offset will be initialized in shift
+require 'pry'
+class Shift
+  attr_reader :rand_number, :date
+  def initialize(rand_number, date)
+    @rand_number = rand_number
+    @date = date
+  end
+
+  def keys
+    assignments = {}
+    assignments["A"] = @rand_number[0..1].to_i
+    assignments["B"] = @rand_number[1..2].to_i
+    assignments["C"] = @rand_number[2..3].to_i
+    assignments["D"] = @rand_number[3..4].to_i
+    assignments
+  end
+
+  x
+end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,8 +3,21 @@ require './lib/shift'
 
 class ShiftTest < Minitest::Test
 
+  def setup
+    @shift = Shift.new("02715", "040895")
+  end
+
   def test_it_exists
-    shift = Shift.new("12345", 041920)
-    assert_equal Shift, shift
+    assert_instance_of Shift, @shift
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "02715", @shift.rand_number
+    assert_equal "040895", @shift.date
+  end
+
+  def test_it_can_create_keys
+
+    assert_equal ({"A" => 02, "B"=> 27, "C" => 71, "D" => 15}), @shift.keys
   end
 end
