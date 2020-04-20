33f3a42613b424e63aa514434d2d12c30765cae7
Sage Lee <sagemcklee@gmail.com>
2020-04-20 14:31:42 -0600

    Add default key and date attributs along with tests

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,36 +1,15 @@
+require './lib/shift'
+require 'date'
 
+class Cipher
+  attr_reader :message, :key, :date, :shift
 
-def initialize(message, key, date)
-  @ message =
-  @alphabet
-  keys = Key.new
-  offset = Offset.new
-end
-
-
-def keys(key)
-  #take in random number
-  #return hash A-D
-end
-
-def offset(date)
-  #take in date
-  #return hash A-D
-end
-
-def shift(key,date)
-  keys(key) + offset(date)
-  return a hash of shifts
-
-end
-
-def change_alphabet_with_shift
-  #change the @alphabet accordin to the shift
-  #will use idex and modulos
-@changed_alphabet =  @alphabet.map |letter|
-  if letter index modulo == 2 , letter + shift[a]
-
-def new message
-  #take every letter from message and change it according to changed alphabet
-
+  def initialize(message, key = rand(100000).to_s.rjust(5, "0"), date = Date.today.strftime("%m%d%y"))
+    @message = message
+    @key = key
+    @date = date
+    @shift = Shift.new(@key, @date)
   end
+
+
+end
---
+++ test/cipher_test.rb
@@ -0,0 +1,42 @@
+require './test/test_helper'
+require './lib/cipher'
+
+class CipherTest < Minitest::Test
+
+  def test_it_exists
+    @cipher = Cipher.new("hello world", "02715", "040895")
+    assert_instance_of Cipher, @cipher
+  end
+
+  def test_it_has_readable_attributes
+    @cipher = Cipher.new("hello world", "02715", "040895")
+    assert_equal "hello world", @cipher.message
+    assert_equal "02715", @cipher.key
+    assert_equal "040895", @cipher.date
+    assert_instance_of Shift, @cipher.shift
+  end
+
+  def test_it_can_produce_todays_date_by_default
+    @cipher = Cipher.new("hello world", "02715")
+    assert_equal 6, @cipher.date.length
+  end
+
+  def test_it_can_produce_rand_key_by_default
+    @cipher = Cipher.new("hello world")
+    assert_equal 6, @cipher.date.length
+    assert_equal 5, @cipher.key.length
+  end
+
+  def test_it_can_encrypt
+    expected =
+    {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @ciper.encrypt
+  end
+
+
+
+end
