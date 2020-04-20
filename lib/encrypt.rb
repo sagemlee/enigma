78236cada7906dea33065c3c19a11c4e2422a1e2
Sage Lee <sagemcklee@gmail.com>
2020-04-20 11:59:48 -0600

    Add command line interfaces for encrypt and decrypt

---
+++ lib/encrypt.rb
@@ -0,0 +1,15 @@
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+puts incoming_text
+
+capitalized_text = incoming_text.upcase
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(capitalized_text)
+
+writer.close
