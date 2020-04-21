6c80dc2968c42eae009bcdeef4836474e5f6759d
Sage Lee <sagemcklee@gmail.com>
2020-04-20 23:08:05 -0600

    Add test it can crack

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,32 +17,12 @@ class EnigmaTest < Minitest::Test
     assert enigma.rand_number.to_i > 0
   end
 
-  def test_it_has_readable_attributes
-    skip
-    enigma = Enigma.new("hello world", "02715", "040895")
-    assert_equal "hello world", enigma.message
-    assert_equal "02715", enigma.key
-    assert_equal "040895", enigma.date
-    assert_instance_of Shift, enigma.shift
-  end
-
   def test_it_generate_todays_date
     enigma = Enigma.new
     assert_instance_of String, enigma.todays_date
     assert_equal 6, enigma.todays_date.length
     assert enigma.todays_date.to_i < 130000
 
-  end
-  #   skip
-  #   enigma = Enigma.new("hello world", "02715")
-  #   assert_equal 6, enigma.date.length
-  # end
-
-  def test_it_can_produce_rand_key_by_default
-    skip
-    enigma = Enigma.new("hello world")
-    assert_equal 6, enigma.date.length
-    assert_equal 5, enigma.key.length
   end
 
   def test_it_can_encrypt
@@ -68,31 +48,31 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_encrypt_without_given_date
-    skip
-    enigma = Enigma.new("hello world", "02715")
+    enigma = Enigma.new
     expected =
     {
       encryption: "jib qdmctpu",
       key: "02715",
       date: "042020"
     }
-    assert_equal expected, enigma.encrypt
+    encrypted = enigma.encrypt("hello world", "02715")
+    assert_equal expected, encrypted
   end
-
-  #enigma.decrypt(encrypted[:encryption], "02715")
 
   def test_it_can_decrypt_without_given_date
     enigma = Enigma.new
+    encrypted = enigma.encrypt("hello world", "02715")
     expected =
     {
       decryption: "hello world",
       key: "02715",
       date: "042020"
     }
-    assert_equal expected, enigma.decrypt("jib qdmctpu", "02715")
+    assert_equal expected, enigma.decrypt(encrypted[:encryption], "02715")
   end
 
   def test_it_can_encrypt_without_given_date_or_key
+    skip
     enigma = Enigma.new
     Date.stubs(:strftime).returns("042021")
     Enigma.stubs(:date).returns("042021")
@@ -104,6 +84,20 @@ class EnigmaTest < Minitest::Test
       date: "042020"
     }
     assert_equal expected, enigma.encrypt("hello world")
+  end
+
+  def test_it_can_crack
+    skip
+    enigma = Enigma.new
+    enigma.encrypt("hello world end", "08304", "291018")
+    expected =
+      {
+        decryption: "hello world end",
+        date: "291018",
+        key: "08304"
+      }
+
+    assert_equal expected, enigma.crack("vjqtbeaweqihssi", "291018")
   end
 
 
